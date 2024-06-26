9fbe8d1cc1946de62da1e3e570aadbc94a64a4ec
CodedMatrix <umuhozaallykhaled@gmail.com>
2024-06-25 18:45:12 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the fucntion of the assigment
+ * Return: return 0
+ */
+
+void print_alphabet(void);
+
+{
+	char c;
+	for (c = 'a' ; c <= 'z'; c++)
+	
+	{
+	
+		_putchar(c);
+	
+	}
+	
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet x10 times
+ *  Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+    char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 26; j++)
+        {
+            _putchar(alphabet[j]);
+        }
+        _putchar('\n');
+    }
+}
